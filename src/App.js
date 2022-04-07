bb2a0ef685d0aefea293365ca3200538f2fc36c5
leminhtri <leminhtri900112@gmail.com>
2022-04-07 15:23:03 +0700

    baitaptonghop tailwind lodash

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import BaiTapTongHop from "./BaiTapTongHop/BaiTapTongHop";
+
+export default function App() {
+  return (
+    <div>
+      <BaiTapTongHop />
+    </div>
+  );
+}
